daf5b4859f3b850b7a3578dba40c055e618b837e
Jhon Christian Ambrad <jambrad123@gmail.com>
2021-07-01 10:58:08 +0800

    Update Account.php

--- Models/Account.php
+++ Models/Account.php
@@ -5,9 +5,9 @@ class Credentials {
     public $Password;
 
     public function __construct() {
-        $this->IntegrationID = "a560af9f-8c5c-4546-969c-6f6d0111401a";
-        $this->Username = "Kaival-001";
-        $this->Password = "May2020!";
+        $this->IntegrationID = "IntegrationIDString";
+        $this->Username = "YourUser";
+        $this->Password = "YourPassword";
     }
 }
 ?>
